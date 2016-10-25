8cc83d3678e335fa9dde805901305d08cd29f813
Robert Szmurlo <robert.szmurlo@ee.pw.edu.pl>
2016-10-25 17:12:33 +0200

    * Potrafimy juz zmieniać opisy * Usuwajac pliki taj naprawd zaznaczamy ze sa usuniete a ich fizycznie nie usuwamy z systemu plikow. * Zapisuja polik w lokalnym repozytorium tworzymy podkatalog z dzisiejsza data.

--- database.py
+++ database.py
@@ -19,177 +19,183 @@ db.session.expire_on_commit = False
 
 
 class User(db.Model):
-	__tablename__ = 'user'
-	id = db.Column(db.Integer, primary_key=True)
-	username = db.Column(db.String(80), unique=True)
-	password = db.Column(db.String(128))
-	salt = db.Column(db.String(5))
-	home = db.Column(db.String(64), unique=True)
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    password = db.Column(db.String(128))
+    salt = db.Column(db.String(5))
+    home = db.Column(db.String(64), unique=True)
 
-	def __init__(self, username, root='/tmp'):
-		self.username = username
-		self.home = '%s/%s' % (root, username)
+    def __init__(self, username, root='/tmp'):
+        self.username = username
+        self.home = '%s/%s' % (root, username)
 
-	def set_password(self, password, salt=None):
-		if salt is None:
-			salt = ''.join(choice(ascii_lowercase + digits) for x in range(10))
-		self.password = sha512(salt + password).hexdigest()
-		self.salt = salt
+    def set_password(self, password, salt=None):
+        if salt is None:
+            salt = ''.join(choice(ascii_lowercase + digits) for x in range(10))
+        self.password = sha512(salt + password).hexdigest()
+        self.salt = salt
 
-	def __repr__(self):
-		return '<User %r>' % self.username
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 
 class UserSessionData(db.Model):
-	uid = db.Column(db.Integer, primary_key=True)
-	user_id = db.Column(db.Integer)
-	key = db.Column(db.String(32))
-	value = db.Column(db.Binary())
+    uid = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer)
+    key = db.Column(db.String(32))
+    value = db.Column(db.Binary())
 
-	def __init__(self, key, value, user_id):
-		self.user_id = user_id
-		self.key = key
-		self.value = value
+    def __init__(self, key, value, user_id):
+        self.user_id = user_id
+        self.key = key
+        self.value = value
 
 
 class Dataset(db.Model):
-	__tablename__ = 'dataset'
-	id = db.Column(db.Integer, primary_key=True)
-	name = db.Column(db.String(128))
+    __tablename__ = 'dataset'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128))
 
-	date_created = db.Column(db.DateTime())
-	user_created_id = db.Column(db.Integer, db.ForeignKey("user.id"))
-	user_created = relationship("User", back_populates="datasets_created", foreign_keys=[user_created_id])
+    date_created = db.Column(db.DateTime())
+    user_created_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    user_created = relationship("User", back_populates="datasets_created", foreign_keys=[user_created_id])
 
-	date_modified = db.Column(db.DateTime())
-	user_modified_id = db.Column(db.Integer, db.ForeignKey("user.id"))
-	user_modified = relationship("User", back_populates="datasets_modified", foreign_keys=[user_modified_id])
+    date_modified = db.Column(db.DateTime())
+    user_modified_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    user_modified = relationship("User", back_populates="datasets_modified", foreign_keys=[user_modified_id])
 
-	type_id = db.Column(db.Integer, db.ForeignKey("dataset_type.id"))
-	type = relationship("DatasetType", back_populates="datasets", foreign_keys=[type_id])
+    type_id = db.Column(db.Integer, db.ForeignKey("dataset_type.id"))
+    type = relationship("DatasetType", back_populates="datasets", foreign_keys=[type_id])
 
-	short_notes = db.Column(db.String(255))
-	long_notes = db.Column(db.String(8192))
+    short_notes = db.Column(db.String(255))
+    long_notes = db.Column(db.String(8192))
 
-	def __init__(self, name, user_created):
-		self.user_created = user_created
-		self.name = name
-		self.date_created = datetime.utcnow()
-		self.date_modified = self.date_created
-		self.user_modified = user_created
+    deleted = db.Column(db.Boolean, default=False)
 
-	def files_by_type(self, file_type_name, parent_uid = None):
-		result = []
+    def __init__(self, name, user_created):
+        self.user_created = user_created
+        self.name = name
+        self.date_created = datetime.utcnow()
+        self.date_modified = self.date_created
+        self.user_modified = user_created
 
-		for f in self.files:
-			if f.type.lower() == file_type_name.lower():
-				if parent_uid is None:
-					result.append(f)
-				else:
-					if f.parent is not None and f.parent.uid == parent_uid:
-						result.append(f)
+    def files_by_type(self, file_type_name, parent_uid=None):
+        result = []
+
+        for f in self.files:
+            if f.deleted is False and f.type.lower() == file_type_name.lower():
+                if parent_uid is None:
+                    result.append(f)
+                else:
+                    if f.parent is not None and f.parent.uid == parent_uid:
+                        result.append(f)
+
+        return result
 
 
-		return result
-
-
-User.datasets_modified = relationship("Dataset", order_by=Dataset.id, back_populates="user_modified", foreign_keys=[Dataset.user_modified_id])
-User.datasets_created = relationship("Dataset", order_by=Dataset.id, back_populates="user_created", foreign_keys=[Dataset.user_created_id])
+User.datasets_modified = relationship("Dataset", order_by=Dataset.id, back_populates="user_modified",
+                                      foreign_keys=[Dataset.user_modified_id])
+User.datasets_created = relationship("Dataset", order_by=Dataset.id, back_populates="user_created",
+                                     foreign_keys=[Dataset.user_created_id])
 
 
 class DatasetType(db.Model):
-	__tablename__ = 'dataset_type'
-	id = db.Column(db.Integer, primary_key=True)
+    __tablename__ = 'dataset_type'
+    id = db.Column(db.Integer, primary_key=True)
 
-	name = db.Column(db.String(128))
-	file_types = db.Column(db.String(8192))
+    name = db.Column(db.String(128))
+    file_types = db.Column(db.String(8192))
 
-	def __init__(self, name):
-		self.name = name
+    def __init__(self, name):
+        self.name = name
 
-	def file_types_list(self, parent):
-		config = json.loads(self.file_types)
-		res = []
-		for type in config['types']:
-			if 'parent_type' not in type or type['parent_type'] == parent:
-				res.append(type)
-		return res
+    def file_types_list(self, parent):
+        config = json.loads(self.file_types)
+        res = []
+        for type in config['types']:
+            if 'parent_type' not in type or type['parent_type'] == parent:
+                res.append(type)
+        return res
 
-DatasetType.datasets = relationship("Dataset", order_by=Dataset.id, back_populates="type", foreign_keys=[Dataset.type_id])
+
+DatasetType.datasets = relationship("Dataset", order_by=Dataset.id, back_populates="type",
+                                    foreign_keys=[Dataset.type_id])
 
 
 class StoredFile(db.Model):
-	uid = db.Column(db.Integer, primary_key=True)
-	path = db.Column(db.String(512))
-	name = db.Column(db.String(128))
-	description = db.Column(db.String(512))
-	content_type = db.Column(db.String(32))
-	type = db.Column(db.String(128))
+    uid = db.Column(db.Integer, primary_key=True)
+    path = db.Column(db.String(512))
+    name = db.Column(db.String(128))
+    description = db.Column(db.String(512))
+    content_type = db.Column(db.String(32))
+    type = db.Column(db.String(128))
 
-	stored_at = db.Column(db.DateTime(), default=datetime.utcnow)
-	stored_by_id = db.Column(db.Integer, db.ForeignKey("user.id"))
-	stored_by = relationship("User", foreign_keys=[stored_by_id])
+    stored_at = db.Column(db.DateTime(), default=datetime.utcnow)
+    stored_by_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    stored_by = relationship("User", foreign_keys=[stored_by_id])
 
-	dataset_id = db.Column(db.Integer, db.ForeignKey("dataset.id"))
-	dataset = relationship("Dataset", back_populates="files")
+    dataset_id = db.Column(db.Integer, db.ForeignKey("dataset.id"))
+    dataset = relationship("Dataset", back_populates="files")
 
-	deleted = db.Column(db.Boolean, default=False)
+    deleted = db.Column(db.Boolean, default=False)
 
-	def __init__(self, file_path, content_type=None):
-		directory, name = os.path.split(file_path)
+    def __init__(self, file_path, content_type=None):
+        directory, name = os.path.split(file_path)
 
-		if len(name) > 0:
-			name, extension = os.path.splitext(name)
-			self.name = name
-			self.path = os.path.join(directory, name + extension)
+        if len(name) > 0:
+            name, extension = os.path.splitext(name)
+            self.name = name
+            self.path = os.path.join(directory, name + extension)
 
-			if content_type is None:
-				content_type = content_type_helper.get_content_type_by_extension(extension)
-				if content_type is None:
-					content_type = 'application/octet-stream'
-			self.content_type = content_type
-		else: # path is a directory (ends with '/')
-			self.name = os.path.join(os.path.basename(directory), '')
-			self.path = directory
-			self.content_type = 'inode/directory'
+            if content_type is None:
+                content_type = content_type_helper.get_content_type_by_extension(extension)
+                if content_type is None:
+                    content_type = 'application/octet-stream'
+            self.content_type = content_type
+        else:  # path is a directory (ends with '/')
+            self.name = os.path.join(os.path.basename(directory), '')
+            self.path = directory
+            self.content_type = 'inode/directory'
 
-	def read(self, charset='latin2'):
-		with open(self.path, 'rb') as f:
-			raw_bytes = f.read()
-			if charset:
-				return raw_bytes.decode(charset)
-			return raw_bytes
+    def read(self, charset='latin2'):
+        with open(self.path, 'rb') as f:
+            raw_bytes = f.read()
+            if charset:
+                return raw_bytes.decode(charset)
+            return raw_bytes
 
-	def __repr__(self):
-		return '<StoredFile (uid: %s, name:"%s", path:"%s") %s>' % (self.uid, self.name, self.path, self.content_type)
+    def __repr__(self):
+        return '<StoredFile (uid: %s, name:"%s", path:"%s") %s>' % (self.uid, self.name, self.path, self.content_type)
 
-	def __str__(self):
-		return self.path
+    def __str__(self):
+        return self.path
+
 
 Dataset.files = relationship("StoredFile", order_by=StoredFile.uid, back_populates="dataset")
 StoredFile.parent_id = db.Column(db.Integer, db.ForeignKey("stored_file.uid"))
 StoredFile.parent = relationship(StoredFile, backref='children', remote_side=StoredFile.uid)
 
+
 class TemporaryStoredFile(StoredFile):
-	content = str()
+    content = str()
 
-	def __init__(self, content, file_name, content_type):
-		self.content = content
+    def __init__(self, content, file_name, content_type):
+        self.content = content
 
-		StoredFile.__init__(self, file_name, content_type)
+        StoredFile.__init__(self, file_name, content_type)
 
-	def read(self):
-		return self.content
+    def read(self):
+        return self.content
 
-	def write(self, directory, charset='latin2'):
-		self.path = os.path.join(directory, self.path)
-		filesystem_helper.mkdir_directories_for(self.path)
-		with open(self.path, 'wb') as f:
-			content = self.read()
-			if charset:
-				content = content.encode(charset)
-			f.write(content)
+    def write(self, directory, charset='latin2'):
+        self.path = os.path.join(directory, self.path)
+        filesystem_helper.mkdir_directories_for(self.path)
+        with open(self.path, 'wb') as f:
+            content = self.read()
+            if charset:
+                content = content.encode(charset)
+            f.write(content)
 
-	def __repr__(self):
-		return '<TemporaryStoredFile %s supposed to be written in %r (%s)>' % (self.name, self.path, self.content_type)
-
+    def __repr__(self):
+        return '<TemporaryStoredFile %s supposed to be written in %r (%s)>' % (self.name, self.path, self.content_type)
--- modules/datastore/datastore.py
+++ modules/datastore/datastore.py
@@ -17,7 +17,7 @@ def datastore():
     if not g.user_id:
         abort(401)
 
-    datasets = database.Dataset.query.order_by(database.Dataset.date_created)
+    datasets = database.Dataset.query.filter_by(deleted=False).order_by(database.Dataset.date_created)
     dataset_types = database.DatasetType.query.order_by(database.DatasetType.name)
     return render_template('datastore/datastore.html', scenarios=g.scenarios, uid=None, datasets=datasets,
                            now=datetime.utcnow(), dataset_types=dataset_types)
@@ -116,8 +116,13 @@ def upload_file():
             parent_file = None
 
         filename = secure_filename("%s_%s" % (file.filename, str(uuid.uuid4())))
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        fullpath_filename = "%s%s" % (app.config['UPLOAD_FOLDER'], filename)
+        datefolder = datetime.utcnow().strftime('%Y-%m-%d')
+        uploadpath = os.path.join(app.config['UPLOAD_FOLDER'], datefolder)
+        if not os.path.isdir(uploadpath):
+            os.mkdir(uploadpath)
+        fullpath_filename = os.path.join(uploadpath, filename)
+        file.save(fullpath_filename)
+
 
         # Zapamiętuję plik w bazie danych
         stored_file = database.StoredFile(fullpath_filename, None)
@@ -145,13 +150,41 @@ def delete_file(uid):
 
     stored_file = database.StoredFile.query.filter_by(uid=uid).one()
     if stored_file is not None:
-        file_name = stored_file.name
-        os.remove(stored_file.path)
         dataset = stored_file.dataset
-        database.db.session.delete(stored_file)
+
+        file_name = stored_file.name
+        #os.remove(stored_file.path)
+        #database.db.session.delete(stored_file)
+
+        stored_file.deleted = True
+        database.db.session.add(stored_file)
         database.db.session.commit()
 
         flash(u"Poprawnie usunąłem plik %s" % file_name, 'success')
+
+    return render_template('datastore/dataset.html', scenarios=g.scenarios, uid=dataset.id, dataset=dataset)
+
+@app.route('/data/update_comment', methods=['POST', 'GET'])
+def update_comment():
+    if not g.user_id:
+        abort(401)
+
+    dataset = None
+
+    args = merge_http_request_arguments(True)
+    file_id = int(args['file_id'])
+    new_comment = args['comment']
+
+    stored_file = database.StoredFile.query.filter_by(uid=file_id).one()
+    if stored_file is not None:
+        dataset = stored_file.dataset
+
+        file_name = stored_file.name
+        stored_file.description = new_comment
+        database.db.session.add(stored_file)
+        database.db.session.commit()
+
+        flash(u"Zakrualizowałem komentarz do pliku %s na wartość: '%s'" % (file_name, new_comment), 'success')
 
     return render_template('datastore/dataset.html', scenarios=g.scenarios, uid=dataset.id, dataset=dataset)
 
